cdc15a7d10dd6d446ea23520e48f30f07a64a893
David Panesiu <davidpanesiu@gmail.com>
2022-01-24 20:22:11 -0700

    Updated landing page component

--- components/MainSection.jsx
+++ components/MainSection.jsx
@@ -9,7 +9,7 @@ const MainSection = () => {
           <span className={styles.brand}>United</span> We Stand
         </h1>
         <p className={styles.description}>Bringing together mayonnaise farmers across the globe to empower the cultivation and distribution of mayonnaise in every world market.</p>
-        <button className={styles.button}>Learn More</button>
+        <button className={`${styles.grow} ${styles.button}`}>Learn More</button>
       </div>
       <div className={styles.secondSection}>
         <div className={styles.card}>
@@ -22,21 +22,25 @@ const MainSection = () => {
             objectFit='contain'
           />
         </div>
-        <div className={styles.card}>
-          <h2 className={styles.subheading}>A partner for the</h2>
-          <p className={styles.description}>lorem ipsum</p>
-          <button className={styles.button}>Learn</button>
+        <div className={styles.cardAlt}>
+          <h2 className={styles.subheading}>Partners with the best mayonnaise producers in the world</h2>
+          <p className={styles.description}>Mayonnaise Farmers Alliance brings 1300+ years of industry experience and empowers mayonnaise farmers to unleash the full potential of thier crop.</p>
+          <button className={`${styles.grow} ${styles.button}`}>Learn More</button>
         </div>
+      </div>
+      <div className={styles.eventBanner}>
+        <h2>Checkout this year's Mayonnaise Festivals!</h2>
+        <button className={styles.grow}>View Events</button>
       </div>
       <div className={styles.thirdSection}>
         <div className={styles.cardAlt}>
           <h2 className={styles.subheading}>Meet the Farmers</h2>
-          <p className={styles.description}>lorem ipsum</p>
-          <button className={styles.button}>Learn</button>
+          <p className={styles.description}>Meet our growers from far and wide that have jumped on board to partner with us.</p>
+          <button className={`${styles.grow} ${styles.button}`}>Learn More</button>
         </div>
         <div className={styles.card}>
           <Image 
-            src='/images/farmers.jpg' 
+            src='/images/farmer.png' 
             alt='' 
             width='100%' 
             height='100%' 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Intro from '../components/Intro'
 import MainSection from '../components/MainSection'
 import styles from '../styles/Home.module.css'
 
